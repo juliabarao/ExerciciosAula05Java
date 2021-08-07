bfb4b04446378531f820f47241df97923ee1d055
Julia <juliarbaraos@gmail.com>
2021-08-06 23:32:11 -0300

    Atualizando nome do exercicio 1

---
+++ src/br/com/generation/poo1/ClientesExercicio1.java
@@ -0,0 +1,14 @@
+package br.com.generation.poo1;
+
+public class ClientesExercicio1 {
+	
+	String nome;
+	String produto;
+	int quantidade;
+	double valor;
+	
+	void pagarProduto() {
+		System.out.println("Clientes e compras: ");
+	}
+	
+}
